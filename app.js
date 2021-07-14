b5ee8fa288f540e6d45884e800fe642717c1f314
msolorio <msolorio.github@gmail.com>
2021-07-13 18:06:11 -0700

    Cleans up code

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
+// SELECT DOM ELEMENTS
 const buttonEl = document.getElementById('button');
 const inputEl = document.getElementById('input');
 const ulEl = document.getElementById('comments-list');
 
+// ANATOMY OF AN EVENT LISTENER
+// domElement.addEventListener('eventType', function () {});
+// - addEventListener is called on the dom element you want to add an event listener to
+// - addEventListener takes 2 arguments
+// -- type of event we want to listen for - for example 'click'
+// -- callback function - the function to call when the event is triggered
 
 
-// When someone clicks on a comment, delete that comment
-// Event delegation
-ulEl.addEventListener('click', function (event) {
-  console.log(event.target);
-
-  // event.target.style.color = 'red';
-  event.target.remove();
-
-  console.log('Clicked on the UL element!!!')
-});
-
-// Add an event listener to our button
-// We want to listen for when someone clicks our button
-//              name of the event, "callback" function
+///////////////////////////////////////////////////////////
+// Listen for when a user clicks on the button
+// Goal: When a user clicks the button, add a new comment
+// to the list
 buttonEl.addEventListener('click', function (event) {
   // Psuedocode
   // Step 1: Get the value typed into the input (store in variable)
@@ -26,14 +23,13 @@ buttonEl.addEventListener('click', function (event) {
   const newComment = document.createElement('li');
 
   // Step 2a: Add a "comment" class to the LI
-  // newComment.setAttribute('class', 'comment');
-  newComment.classList.add('comment');
+  newComment.setAttribute('class', 'comment');
+  // newComment.classList.add('comment');
 
-  console.log(newComment);
   // Step 3: Insert the input value into the new LI
   newComment.textContent = inputVal;
+  // console.log(newComment);
 
-  console.log(newComment);
   // Step 4: Append the LI element to the page inside the UL
   ulEl.append(newComment);
 
@@ -42,12 +38,31 @@ buttonEl.addEventListener('click', function (event) {
 });
 
 
+///////////////////////////////////////////////////////////
+// Goal: When someone clicks on a comment, delete that comment
+// Referred to as "Event Delegation"
+ulEl.addEventListener('click', function (event) {
+  console.log(event.target);
 
+  // event.target.style.color = 'red';
+  event.target.remove();
+
+  // console.log('Clicked on the UL element!!!')
+});
+
+
+///////////////////////////////////////////////////////////
+// Extra Credit:
+// Goal: When the user presses the "Enter" key on the input
+// Add a new comment to the list
+
+// Listen for a 'keypress' event on our input element
 inputEl.addEventListener('keypress', function (event) {
   // console.log(event);
 
   // console.log(event.code);
 
+  // If the 'Enter' key was pressed, add comment to the list
   if (event.code === 'Enter') {
     // Step 1: Get the value typed into the input (store in variable)
     const inputVal = inputEl.value;
@@ -58,17 +73,14 @@ inputEl.addEventListener('keypress', function (event) {
     // newComment.setAttribute('class', 'comment');
     newComment.classList.add('comment');
 
-    console.log(newComment);
     // Step 3: Insert the input value into the new LI
     newComment.textContent = inputVal;
 
-    console.log(newComment);
+    // console.log(newComment);
     // Step 4: Append the LI element to the page inside the UL
     ulEl.append(newComment);
 
     // Step 5: Clear out the input
     inputEl.value = '';
   }
-
-  // console.log('Pressed a key');
 })
